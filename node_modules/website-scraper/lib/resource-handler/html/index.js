345ee9a261e0b81084c5434848d7c640425b04e9
sachin saini <sachin saini>
2017-04-26 15:14:16 +0530

    first commit

---
+++ node_modules/website-scraper/lib/resource-handler/html/index.js
@@ -0,0 +1,54 @@
+var cheerio = require('cheerio');
+var Promise = require('bluebird');
+var utils = require('../../utils');
+var HtmlSourceElement = require('./html-source-element');
+
+function HtmlResourceHandler (options, handleChildrenPaths) {
+	this.options = options;
+	this.handleChildrenPaths = handleChildrenPaths;
+}
+
+HtmlResourceHandler.prototype.handle = function handle (resource) {
+	var $ = loadTextToCheerio(resource.getText());
+	prepareToLoad($, resource);
+
+	return Promise.mapSeries(this.options.sources, this.loadResourcesForRule.bind(this, $, resource))
+		.then(function updateResource () {
+			resource.setText($.html());
+			return resource;
+		});
+};
+
+HtmlResourceHandler.prototype.loadResourcesForRule = function loadResourcesForRule ($, parentResource, rule) {
+	var self = this;
+	var promises = $(rule.selector).map(function loadForElement () {
+		var el = new HtmlSourceElement($(this), rule);
+		var pathContainer = el.getPathContainer();
+		if (!pathContainer) {
+			return Promise.resolve();
+		}
+		return self.handleChildrenPaths(pathContainer, parentResource).then(el.setData.bind(el));
+	}).get();
+
+	return utils.waitAllFulfilled(promises);
+};
+
+function prepareToLoad ($, resource) {
+	$('base').each(function handleBaseTag () {
+		var el = $(this);
+		var href = el.attr('href');
+		if (href) {
+			var newUrl = utils.getUrl(resource.getUrl(), href);
+			resource.setUrl(newUrl);
+			el.remove();
+		}
+	});
+}
+
+function loadTextToCheerio (text) {
+	return cheerio.load(text, {
+		decodeEntities: false
+	});
+}
+
+module.exports = HtmlResourceHandler;
---
+++ node_modules/website-scraper/lib/resource-handler/index.js
@@ -0,0 +1,83 @@
+var _ = require('lodash');
+var Promise = require('bluebird');
+var logger = require('../logger');
+var utils = require('../utils');
+
+var HtmlHandler = require('./html');
+var CssHandler = require('./css');
+
+var supportedOptions = ['prettifyUrls', 'sources', 'defaultFilename'];
+
+function ResourceHandler (options, context) {
+	var self = this;
+	self.options = _.pick(options, supportedOptions);
+	self.context = context;
+
+	self.htmlHandler = new HtmlHandler(self.options, self.handleChildrenResources.bind(self));
+	self.cssHandler = new CssHandler(self.options, self.handleChildrenResources.bind(self));
+}
+
+ResourceHandler.prototype.getResourceHandler = function getResourceHandler (resource) {
+	switch (true) {
+		case resource.isCss():
+			logger.debug('using css handler for ' + resource);
+			return this.cssHandler;
+		case resource.isHtml():
+			logger.debug('using html handler for ' + resource);
+			return this.htmlHandler;
+		default:
+			logger.debug('using no handler for ' + resource);
+			return null;
+	}
+};
+
+/**
+ * Request all resources from pathContainers paths
+ * @param pathContainer - instance of ImgSrcsetTag or CommonTag or CssText, contains original paths for resources
+ * @param {Resource} parentResource
+ * @returns {Promise} - resolved when all resources from pathContainer were requested
+ * and original paths in parentResource were updated with local paths for children resources
+ */
+ResourceHandler.prototype.handleChildrenResources = function handleChildrenResources (pathContainer, parentResource) {
+	var self = this;
+	var childrenPaths = pathContainer.getPaths();
+	var pathsToUpdate = [];
+
+	var childrenPromises = childrenPaths.map(function loadChildPath (childPath) {
+		var childResourceUrl = utils.getUrl(parentResource.getUrl(), childPath);
+		var childResource = parentResource.createChild(childResourceUrl);
+
+		return self.context.requestResource(childResource).then(function updateChildPath (respondedResource) {
+			if (respondedResource) {
+				parentResource.updateChild(childResource, respondedResource);
+
+				var relativePath = utils.getRelativePath(parentResource.getFilename(), respondedResource.getFilename());
+				if (self.options.prettifyUrls) {
+					relativePath = relativePath.replace(self.options.defaultFilename, '');
+				}
+				var hash = utils.getHashFromUrl(childPath);
+
+				if (hash) {
+					relativePath = relativePath.concat(hash);
+				}
+
+				pathsToUpdate.push({ oldPath: childPath, newPath: relativePath});
+			}
+			return null; // Prevent Bluebird warnings
+		});
+	});
+
+	return utils.waitAllFulfilled(childrenPromises).then(function updateChildrenPaths () {
+		return pathContainer.updateText(pathsToUpdate);
+	});
+};
+
+ResourceHandler.prototype.handleResource = function handleResource (resource) {
+	var resourceHandler = this.getResourceHandler(resource);
+	if (resourceHandler && resourceHandler.handle) {
+		return resourceHandler.handle(resource);
+	}
+	return Promise.resolve(resource);
+};
+
+module.exports = ResourceHandler;
